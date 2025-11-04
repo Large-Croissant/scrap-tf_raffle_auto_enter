df601533854d864e58d9c939cb730814aa899897
Joey <jpg2368@rit.edu>
2025-11-04 13:49:11 -0500

    Changed logging to use logging module, fixed detection of buttons and success/failure

--- main.py
+++ main.py
@@ -2,31 +2,34 @@ import playwright
 import playwright.sync_api
 import time
 import random
-import sys
 import logging
 
+
 def rand_wait():
-    time.sleep(random.randrange(3, 12)/10)
+    rand_time = random.randrange(3, 12) / 10
+    logging.debug(f"Waiting for {rand_time} seconds")
+    time.sleep(rand_time)
 
 
 def main():
     with playwright.sync_api.sync_playwright() as p:
         # Launch the browser (headless=False to see the browser window)
         browser = p.chromium.launch(headless=False).new_context()
-        
+
         page = browser.new_page()
 
         # Navigate to a webpage
         page.goto("https://scrap.tf/login")
-        print("Navigated to login page", file=sys.stderr, flush=True)
+        logging.info("Navigated to login page")
 
-        input("Press <Enter> when logged in...")
-        print("User logged in", file=sys.stderr, flush=True)
+        page.wait_for_url("https://scrap.tf", timeout=0)
+        logging.info("User logged in")
+        rand_wait()
 
         page.goto("https://scrap.tf/raffles")
-        print("Navigated to raffles page", file=sys.stderr, flush=True)
+        logging.info("Navigated to public raffles page")
         page.wait_for_selector("#raffles-list")
-        print("Raffles found", file=sys.stderr, flush=True)
+        logging.info("Raffles found")
 
         all_raffles = page.query_selector_all("#raffles-list .panel-raffle")
         unentered_raffle_links = []
@@ -34,23 +37,30 @@ def main():
             if "raffle-entered" not in raffle.get_attribute("class"):
                 href = raffle.query_selector(".raffle-name a").get_attribute("href")
                 unentered_raffle_links.append(f"https://scrap.tf{href}")
-                print(f"Raffle https://scrap.tf{href} found", file=sys.stderr, flush=True)
-        
+                logging.info(f"Raffle https://scrap.tf{href} found")
+
         rand_wait()
 
         for link in unentered_raffle_links:
             raffle_page = browser.new_page()
             raffle_page.goto(link)
-            print(f"Navigated to raffle {link}", file=sys.stderr, flush=True)
-            raffle_page.wait_for_selector("#raffle-enter", state="visible")
-            print("Enter raffle button found", file=sys.stderr, flush=True)
-            rand_wait()
-            raffle_page.click("#raffle-enter")
-            print("Clicked enter raffle button", file=sys.stderr, flush=True)
-            raffle_page.wait_for_selector("#raffle-leave")
-            print("Raffle enter successful", file=sys.stderr, flush=True)
-            rand_wait()
-            print("Leaving page", file=sys.stderr, flush=True)
+            logging.info(f"Navigated to raffle {link}")
+            enter_button = raffle_page.wait_for_selector("button:has-text('Enter Raffle'):visible")
+            if enter_button:
+                logging.info("Raffle enter button found")
+                rand_wait()
+                logging.info("Attempting to enter raffle")
+                enter_button.click()
+                logging.info("Clicked button to enter raffle")
+                try:
+                    raffle_page.wait_for_selector("button:has-text('Leave Raffle')", timeout=18000)
+                    logging.info("Raffle successfully entered")
+                    rand_wait()
+                except Exception:
+                    logging.warning("Timed out waiting for raffle enter")
+            else:
+                logging.warning("Couldn't find enter raffle button")
+            logging.info("Leaving raffle page")
             raffle_page.close()
 
         input("<Enter> to close")
@@ -58,4 +68,5 @@ def main():
 
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s", handlers=[logging.StreamHandler()])
     main()
